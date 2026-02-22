5626793f2db8eb19281343efd2b17a3e219289db
giorggaze <giorgilagvilava2709@gmail.com>
2026-02-22 22:18:09 +0400

    second chans

---
+++ GOA_homehowk0/LVL 4/part_5.py
@@ -0,0 +1,12 @@
+
+num1 = float(input("შეიყვანე პირველი რიცხვი: "))
+num2 = float(input("შეიყვანე მეორე რიცხვი: "))
+num3 = float(input("შეიყვანე მესამე რიცხვი: "))
+num4 = float(input("შეიყვანე მეოთხე რიცხვი: "))
+num5 = float(input("შეიყვანე მეხუთე რიცხვი: "))
+
+max_number = max(num1, num2, num3, num4, num5)
+
+print("ყველაზე დიდი რიცხვია:", max_number)
+
+# ადრე ვისწავლე BroCode_ ში
